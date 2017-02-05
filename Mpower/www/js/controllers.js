b9d2a180906b91005ce53bb0ba07c07852b2c64f
jessepf <jessepfrancis@provisionasia.org>
2017-02-06 03:39:05 +0530

    typeface experiments, minor fixes

--- Mpower/www/js/controllers.js
+++ Mpower/www/js/controllers.js
@@ -175,6 +175,7 @@ function ($scope, $stateParams, basicServices, $http, CacheFactory, $sce, $timeo
 	
 	var wpLoader = function( type ) {
 		var cache;
+		var first_done = false;
 		if(postId) {
 			if($rootScope.loaded_online[postId] && postCache.get(postId)) cache = true;
 			else cache = false;
@@ -186,6 +187,11 @@ function ($scope, $stateParams, basicServices, $http, CacheFactory, $sce, $timeo
 			if(postId) $scope.post.push(postCache.get(postId));
 			else $scope.post = postCache.get(type+page);
 			$scope.post.forEach(function(post) {
+				if(first_done && !((postType == 'challenge') || (postType == 'toolkit'))) post.title_size='1.7em';
+				else {
+					post.title_size='2.2em';
+					first_done=true;
+				}
 				if(view_post && (post.image>0) && !imCache.get(post.image)) {
 					var media_link = baseUrl + 'media?include='+post.image+'&_query=[*].media_details.sizes.medium.source_url';
 					$http.get( media_link , http_ops ).then(function(image) {
@@ -213,6 +219,11 @@ function ($scope, $stateParams, basicServices, $http, CacheFactory, $sce, $timeo
 					var id = post.id;
 					if(type == 'toolkit' && id == 268) post.internal_link = '#/content/challenge';
 					else post.internal_link='#/post/' + type + '/' + id;
+					if(first_done && !((postType == 'challenge') || (postType == 'toolkit'))) post.title_size='1.7em';
+					else {
+						post.title_size='2.2em';;
+						first_done=true;
+					}
 					if(view_post) var i = $scope.post.length;
 					if(view_post) $scope.post.push(post);
 					postCache.remove(id);
@@ -254,6 +265,11 @@ function ($scope, $stateParams, basicServices, $http, CacheFactory, $sce, $timeo
 						$http.get('assets/toolkit/' + type + '_meta.json', http_ops ).then(function(offline) {
 							$scope.post = offline.data;
 							$scope.post.forEach(function(post) {
+								if(first_done && !((postType == 'challenge') || (postType == 'toolkit'))) post.title_size='1.7em';
+								else {
+									post.title_size='2.2em';
+									first_done=true;
+								}
 								if(type == 'toolkit' && post.id == 268) post.internal_link = '#/content/challenge';
 								else post.internal_link='#/post/' + type + '/' + post.id;
 								postCache.put(post.id,post);
