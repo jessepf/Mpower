3173bb1ed9d80e4adafbf6e56f251164fe6e133d
jessepf <jessepfrancis@provisionasia.org>
2017-02-06 20:37:54 +0530

    option to hide short excerpt, fixed share text

--- Mpower-android-build/platforms/android/build/generated/source/buildConfig/release/org/provisionasia/mpower/BuildConfig.java
+++ Mpower-android-build/platforms/android/build/generated/source/buildConfig/release/org/provisionasia/mpower/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.provisionasia.mpower";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 9902;
-  public static final String VERSION_NAME = "0.99.2";
+  public static final int VERSION_CODE = 9903;
+  public static final String VERSION_NAME = "0.99.3";
 }
