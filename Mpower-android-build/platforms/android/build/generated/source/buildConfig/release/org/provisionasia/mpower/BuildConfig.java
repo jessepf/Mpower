c7c1c6280d8bcbbdcc7e70f54eb831c5982af304
jessepf <jessepfrancis@provisionasia.org>
2017-02-06 23:17:57 +0530

    version change

--- Mpower-android-build/platforms/android/build/generated/source/buildConfig/release/org/provisionasia/mpower/BuildConfig.java
+++ Mpower-android-build/platforms/android/build/generated/source/buildConfig/release/org/provisionasia/mpower/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.provisionasia.mpower";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 9905;
-  public static final String VERSION_NAME = "0.99.5-rc1";
+  public static final int VERSION_CODE = 9906;
+  public static final String VERSION_NAME = "0.99.6-rc2";
 }
